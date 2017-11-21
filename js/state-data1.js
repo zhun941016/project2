f9b256e3f5f619fdf42f22402e79761f386d7972
zhun941016 <31423404+zhun941016@users.noreply.github.com>
2017-11-20 20:07:51 -0500

    project2

---
+++ js/state-data1.js
@@ -0,0 +1,90 @@
+var stateData = {
+	usa: {
+		name: 'Fate Grand Order',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+
+	},
+	al: {
+		name: 'Archer',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+
+	},
+	ak: {
+		name: 'Caster',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+
+	},
+	az: {
+		name: 'Assassin',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+
+	},
+	ar: {
+		name: 'Rider',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+
+	},
+	ca: {
+		name: 'Saber',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+	},
+	co: {
+		name: 'Lancer',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+
+	},
+	ct: {
+		name: 'Berserker',
+		abbr: 'Support Roles',
+		capitol: 'Gifted with extreme agility',
+		pop: 'Crazed warriors',
+		statehood: 'Excellent scouts',
+		mammal: 'Agile in close quarters',
+		bird: 'Experts of the mount',
+		tree:'Skilled at stealthy operations',
+
+	},
+
+};
